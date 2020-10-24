348ffaa6ce614bf059e8a4872026310d83d9bc78
proland97 <43237966+proland97@users.noreply.github.com>
2020-10-24 09:28:48 +0200

    Login frontend (#5)
    * login page
    
    * mongoDB test data

--- sporttracking-app/components/LoginForm.tsx
+++ sporttracking-app/components/LoginForm.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { Card, Text } from '@ui-kitten/components';
+import React, { useState } from 'react';
+import { Text, Card, Button, Input } from '@ui-kitten/components';
 import { StyleSheet, View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage'
 import { Props } from '@ui-kitten/components/devsupport/services/props/props.service';
+import axios from 'axios';
 
 const RegisterLinkFooter = ({ navigation }: Props) => {
     return (
@@ -18,6 +20,52 @@ const RegisterLinkFooter = ({ navigation }: Props) => {
 };
 
 export const LoginForm = ({ navigation }: Props) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginFailed, setLoginFailed] = useState(false);
+
+    const saveData = async (token : string) => {
+        try {
+          await AsyncStorage.setItem( 'access-token', token)
+        } catch (e) {
+          console.log(e);
+        }
+    }
+
+    const isLoginButtonDisabled = () => {
+        return (
+            password.length === 0 || email.length === 0 || !validateEmail(email)
+        );
+    };
+
+    const validateEmail = (email: string) => {
+        const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+
+        return expression.test(String(email).toLowerCase());
+    };
+
+    const loginUser = async (userToRegister: object) => {
+
+        axios
+            .post('http://localhost:8080/login', userToRegister)
+            .then((res) => {
+                saveData(res.headers.authorization);
+                setLoginFailed(false);
+                console.log('Login was successful!');
+                //TODO redirect user to home page
+                // navigation.navigate('Home')
+            })
+            .catch((err) => {
+                console.log(err);
+                setLoginFailed(true);
+            });
+    };
+
+    const onSubmit = async () => {
+        await loginUser({ email, password });
+    }
+
     const Header = (props: any) => (
         <View {...props}>
             <Text category="h6">SportTracking</Text>
@@ -25,7 +73,15 @@ export const LoginForm = ({ navigation }: Props) => {
     );
 
     const Footer = (props: any) => (
-        <View {...props} style={[props.style, styles.footerContainer]}></View>
+        <View {...props} style={[props.style, styles.footerContainer]}>
+                <Button
+                disabled={isLoginButtonDisabled()}
+                style={styles.footerControl}
+                size="small"
+                status="basic"
+                onPress={onSubmit}
+            >Login</Button>
+        </View>
     );
 
     return (
@@ -36,11 +92,27 @@ export const LoginForm = ({ navigation }: Props) => {
                 header={Header}
                 footer={Footer}
             >
-                <Text category="s2">Implement a form here</Text>
-
-                {
-                    // TODO: IMPLEMENT ME
-                }
+                <Input
+                    style={styles.field}
+                    value={email}
+                    label="Email address"
+                    placeholder="Email address"
+                    secureTextEntry={false}
+                    onChangeText={(val : any) => setEmail(val)}
+                />
+                <Input
+                    style={styles.field}
+                    value={password}
+                    label="Password"
+                    placeholder="Password"
+                    secureTextEntry={true}
+                    onChangeText={(val : any) => setPassword(val)}
+                />
+                {loginFailed && (
+                    <Text style={styles.errorMessage}>
+                        Invalid email or password!
+                    </Text>
+                )}
             </Card>
             <RegisterLinkFooter navigation={navigation} />
         </>
@@ -66,11 +138,18 @@ const styles = StyleSheet.create({
     footerControl: {
         marginHorizontal: 2
     },
+    field: {
+        marginBottom: 20
+    },
     registerLink: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'flex-end',
         paddingBottom: 20,
         backgroundColor: 'white'
-    }
+    },
+    errorMessage: {
+        color: 'red',
+        textAlign: 'center'
+    },
 });
