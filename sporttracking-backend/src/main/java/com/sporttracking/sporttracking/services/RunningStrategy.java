243311615a09c1fc760a26de01aaa6a37e70c88e
proland97 <peter.r.roland@gmail.com>
2020-11-04 19:36:55 +0100

    strategy pattern for calorie calculator

---
+++ sporttracking-backend/src/main/java/com/sporttracking/sporttracking/services/RunningStrategy.java
@@ -0,0 +1,9 @@
+package com.sporttracking.sporttracking.services;
+
+public class RunningStrategy implements CalorieCalculatorStrategy{
+
+    @Override
+    public long calculate(long duration, long weight) {
+        return (long) (duration * 8 * 3.5 * weight / 200);
+    }
+}
